52ca8db9430a8809154079a33a6328e9fd7c73cf
theoamonteiro <theoalvesmonteiro@gmail.com>
2023-05-25 16:46:10 +0100

    feat: show error dialog of multiple devices

--- src/main/python/pasthly.py
+++ src/main/python/pasthly.py
@@ -82,57 +82,15 @@ class Pasthly(GObject.GObject, Nautilus.MenuProvider, Nautilus.LocationWidgetPro
 
     def __init__(self, logger=None):
         self.accel_group = Gtk.AccelGroup()
-        keyval, modifier = Gtk.accelerator_parse('<Shift><Control>v')
+        keyval, modifier = Gtk.accelerator_parse('<Shift><Control>V')
         self.accel_group.connect(keyval, modifier, Gtk.AccelFlags.VISIBLE,
-                                         self._shortcuts_handler)
+                                         self._handle_shortcut)
         self.window = None
         self.destination = None
         self.logger = logger or logging.getLogger(__name__)
 
-    def _shortcuts_handler(self, *args):
-        return self.handle_paste()
-
-    def handle_paste(self):
-        files = self.files_from_clipboard()
-        if not self.destination:
-            dialog = Gtk.MessageDialog(
-                transient_for=self.window,
-                flags=0,
-                message_type=Gtk.MessageType.ERROR,
-                buttons=Gtk.ButtonsType.CANCEL,
-                text="Unexpected State",
-            )
-            dialog.format_secondary_text("PasthlY does not know where to paste the hard links")
-            dialog.run()
-            dialog.destroy()
-            return False
-        duplicates = self.paste_as_hard_link(files, self.destination)
-        if duplicates:
-            dialog = Gtk.MessageDialog(
-                transient_for=self.window,
-                flags=0,
-                message_type=Gtk.MessageType.ERROR,
-                buttons=Gtk.ButtonsType.CANCEL,
-                text="PasthlY doesn't overwrite",
-            )
-            dialog.format_secondary_text(
-                '\n'.join(["Those files already exist:"] + [str(d.absolute()) for d in duplicates])
-            )
-            dialog.run()
-            dialog.destroy()
-            return False
-        return True
-
-    def files_from_clipboard(self):
-        clipboard = Gtk.Clipboard.get(Gdk.SELECTION_CLIPBOARD)
-        lines = clipboard.wait_for_text()
-        if lines is None:
-            return []
-        return [file for file in [Path(line) for line in lines.split('\n')] if file.exists()]
-        
-
     def get_file_items(self, window, files) -> list[Nautilus.MenuItem]:
-        return None # Do not show whit files selected
+        return None # Do not show while files selected
 
     def get_background_items(self, window, folder) -> list[Nautilus.MenuItem]:
         path = self.extract_path(folder)
@@ -142,12 +100,63 @@ class Pasthly(GObject.GObject, Nautilus.MenuProvider, Nautilus.LocationWidgetPro
                                          label='Paste as Hard Link', 
                                          tip='<Shift><Control>V',
                                          icon='')
-        menuitem.sensitive = False
-        menuitem.connect('activate', self._click_handler)
+        menuitem.connect('activate', self._handle_click)
         return [menuitem]
 
-    def _click_handler(self, widget):
+    def get_widget(self, uri, window):
+        if self.window:
+            self.window.remove_accel_group(self.accel_group)
+        window.add_accel_group(self.accel_group)
+        self.window = window
+        return None
+
+    ###
+
+    def _handle_shortcut(self, *args):
         return self.handle_paste()
+
+    def _handle_click(self, widget):
+        return self.handle_paste()
+
+    def handle_paste(self):
+        files = self.files_from_clipboard()
+        try:
+            self.paste_as_hard_link(files, self.destination)
+        except PasthlyError as e:
+            self.handle_error(e)
+            return False
+        return True
+
+    def handle_error(self, error):
+        dialog = Gtk.MessageDialog(
+            transient_for=self.window,
+            flags=0,
+            message_type=Gtk.MessageType.ERROR,
+            buttons=Gtk.ButtonsType.CANCEL,
+            text=error.title if error.title else 'Error',
+        )
+        dialog.format_secondary_text(error.message)
+        dialog.run()
+        dialog.destroy()
+        return error.code
+
+    def handle_duplicates(self):
+        pass # TODO Issue #7
+
+    def files_from_clipboard(self):
+        clipboard = Gtk.Clipboard.get(Gdk.SELECTION_CLIPBOARD)
+        lines = clipboard.wait_for_text()
+        if lines is None:
+            return []
+        lines = lines.split('\n')
+        files = [file for file in [Path(line) for line in lines] if file.exists()]
+        if not files:
+            return []
+        mismatches = [f for f in files if f.parent != files[0].parent]
+        if mismatches:
+            self.logger.warn("Files from diferent parent folder: %s", mismatches)
+            return []
+        return files
 
     def extract_path(self, folder):
         if not folder:
@@ -169,28 +178,44 @@ class Pasthly(GObject.GObject, Nautilus.MenuProvider, Nautilus.LocationWidgetPro
             path = path.get_path()
         return Path(path)
 
-    def get_widget(self, uri, window):
-        if self.window:
-            self.window.remove_accel_group(self.accel_group)
-        window.add_accel_group(self.accel_group)
-        self.window = window
-        return None
-
     def paste_as_hard_link(self, files, target):
+        if not target:
+            raise PasthlyError(code=1, title="Unexpected State", message="PasthlY does not know where to paste the hard links")
         duplicated = []
         destinations = []
+        origin_dir = None
         for file in files:
+            if file.stat().st_dev != target.stat().st_dev:
+                message = 'It is not possible to make hard links from a different device.'
+                raise PasthlyError(message, code=4, title='Different filesystems')
+            origin_dir = origin_dir if origin_dir else file.parent
+            if origin_dir.absolute() != file.parent.absolute():
+                message = f"The file '{file}' is not on the same folder of the other files on the clipboard."
+                raise PasthlyError(message, code=3, title='Multiple source folders')
             destination = target / file.name
             if destination.exists():
                 duplicated.append(destination)
             else:
                 destinations.append((destination, file))
         if duplicated:
-            return duplicated
-        for link, file in destinations:
-            link.hardlink_to(file)
+            text = '\n'.join(["Those files already exist:"] + [str(d.absolute()) for d in duplicated])
+            raise PasthlyError(text, code=2, title="PasthlY doesn't overwrite")
+        try:
+            for link, file in destinations:
+                link.hardlink_to(file)
+        except OSError as ose:
+            message = "Could not finish the creation of the hard links"
+            raise PasthlyError(message, code=10, title='Unexpected Error') from ose
         return []
-        
+
+class PasthlyError(Exception):
+
+    def __init__(self, message, code=-1, title=None):
+        super(PasthlyError, self).__init__(message)
+        self.code = code
+        self.title = title
+        self.message = message
+
 
 def install(logger=None):
     logger = logger if logger else logging.getLogger(__name__)
