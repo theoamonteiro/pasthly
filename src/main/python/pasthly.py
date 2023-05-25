70319339f33cb3b5e73fefeae19ae396e7a2db45
Theo A. Monteiro <theoamonteiro@users.noreply.github.com>
2023-05-25 12:03:21 +0100

    Merge issues/2 into master (#11)
    feat: create a CLI installer
    
    Solves #2

--- src/main/python/pasthly.py
+++ src/main/python/pasthly.py
@@ -1,18 +1,29 @@
 #!/usr/bin/env python3
 import sys
 import os
+import logging
+import logging.config
 from datetime import datetime
 from pathlib import Path
 import gi
 gi.require_version('Gtk', '3.0')
 gi.require_version('Nautilus', '3.0')
 from gi.repository import Nautilus, GObject, Gtk, Gdk, GLib
-import logging
-import logging.config
 from yaml import safe_load
 
+
+SCRIPT_NAME = 'pasthly.py'
+logger = None
+locations = [
+        Path('/usr/share/nautilus-python/extensions'),
+        Path('/usr/share/ubuntu/nautilus-python/extensions'),
+        Path('/usr/share/gnome/nautilus-python/extensions'),
+        Path('/usr/local/share/nautilus-python/extensions'),
+        Path(os.getenv('HOME')) / '.local/share/nautilus-python/extensions' if os.getenv('HOME') else None,
+    ]
+
 __NAUTILUS_PYTHON_DEBUG = os.getenv('NAUTILUS_PYTHON_DEBUG', None)
-if __NAUTILUS_PYTHON_DEBUG == 'misc':
+if __NAUTILUS_PYTHON_DEBUG == 'misc' or __name__ == '__main__':
     log_configs = {
         'version': 1,
         'disable_existing_loggers': False,
@@ -36,7 +47,7 @@ if __NAUTILUS_PYTHON_DEBUG == 'misc':
         }
     }
     logging.config.dictConfig(log_configs)
-    logger = logging.getLogger(__name__)
+    logger = logger if logger else logging.getLogger(__name__)
 
     locations = [
         Path('/usr/share/nautilus-python/extensions'),
@@ -64,7 +75,7 @@ if __NAUTILUS_PYTHON_DEBUG == 'misc':
                 data = safe_load(file)
             log_configs = log_configs | data
             logging.config.dictConfig(log_configs)
-    logger.info('Final log configs: %s', log_configs)
+    logger.debug('Final log configs: %s', log_configs)
 
 
 class Pasthly(GObject.GObject, Nautilus.MenuProvider, Nautilus.LocationWidgetProvider):
@@ -181,8 +192,44 @@ class Pasthly(GObject.GObject, Nautilus.MenuProvider, Nautilus.LocationWidgetPro
         return []
         
 
-def main():
+def install(logger=None):
+    logger = logger if logger else logging.getLogger(__name__)
+    self = Path(__file__)
+    for folder in locations:
+        if not folder.exists():
+            logger.info("'%s' doesn't exist.", folder)
+            continue
+        script = folder / SCRIPT_NAME
+        if script.exists():
+            logger.info("PasthlY (apparently) alreay installed at '%s'", script)
+            return 0
+        if not os.access(folder, os.W_OK):
+            logger.info("'%s' is not writable, skimping it.", folder)
+            continue
+        try:
+            script.write_text(self.read_text())
+            logger.info("PasthlY installed at '%s'", script)
+            return 0
+        except:
+            logger.exception("Couldn't copy '%s' to '%s'", self, script)
+            continue
+    logger.error("No avaliable location for instalation.")
+    if not locations[-1]:
+        logger.info("TIP: try to create '%s'", locations[-1])
     return 0
+
+def main():
+    import argparse
+
+    parser = argparse.ArgumentParser(
+        prog=SCRIPT_NAME,
+        description='A Python "Paste As Hard Link" Nautilus Extension',
+        epilog='See https://github.com/theoamonteiro/pasthly')
+    parser.add_argument('--install', action='store_true', required=True)
+
+    args = parser.parse_args(sys.argv[1:])
+
+    return install()
 
 
 if __name__ == '__main__':
